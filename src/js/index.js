b8d0120e4908e1411d8803a4233d25657d3db911
Kamila <milka107@interia.pl>
2020-01-09 15:40:48 +0100

    Toggle navbar style on scroll

--- src/js/index.js
+++ src/js/index.js
@@ -9,3 +9,20 @@ hamburger.addEventListener('click', () => {
     ? nav.classList.add('nav-show')
     : nav.classList.remove('nav-show');
 });
+
+document.addEventListener('scroll', () => {
+    const scrollValue = window.scrollY;
+    const h1 = document.querySelector('.hero-header').offsetTop;
+    if (scrollValue <= h1) {
+        nav.classList.add('nav-oblique');
+        nav.classList.remove('navbar-default');
+        hamburger.style.transform = 'translateX(-60px)';
+        nav.classList.remove('nav-show');
+        hamburger.classList.remove('menu-is-active');
+
+    } else {
+        nav.classList.remove('nav-oblique');
+        nav.classList.add('navbar-default');
+        hamburger.style.transform = 'translateX(60px)';
+    }
+});
