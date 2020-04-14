0aaf9a470104f9c97f754fd7de773dfec1e60b2f
Kamila <milka107@interia.pl>
2020-04-14 09:28:52 +0200

    Add webpack dev and prod config files, add HtmlWebpackPlugin

--- webpack.common.js
+++ webpack.common.js
@@ -1,11 +1,11 @@
 const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = {
 	entry: './src/js/index.js',
-  output: {
-    filename: 'main.js',
-    path: path.resolve(__dirname, 'dist'),
-  },
+  plugins: [new HtmlWebpackPlugin({
+    template: "./public/index.html"
+  })],
   module: {
     rules: [
       {
